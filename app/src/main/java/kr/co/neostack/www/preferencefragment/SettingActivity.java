6d60f5d218f711ba92a44b699b327a266c11e552
songtaegeon <46990311+songtaegeon@users.noreply.github.com>
2019-03-06 15:13:31 +0900

    sharedpreference 의 fragment로 dialog box 형식으로 구성

---
+++ app/src/main/java/kr/co/neostack/www/preferencefragment/SettingActivity.java
@@ -0,0 +1,13 @@
+package kr.co.neostack.www.preferencefragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
